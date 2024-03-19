b7cc27be109b78076978435ebdb3ca08d882412a
Manojit Das <manojitdas180306@gmail.com>
2024-03-20 01:10:21 +0530

    changing in config file

--- PROJECT/RazorPay Clone/tailwind.config.js
+++ PROJECT/RazorPay Clone/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["./src/pages//*.{html,js}", "./src/components//.{html,js}", "./build/server/pages/**/.{html,js}"],
   theme: {
     extend: {fontFamily: {
       mullish: ["Mulish", "sans-serif"],
