d12e9595954f970f8ca3d313d95e72018e47d160
Manojit Das <manojitdas180306@gmail.com>
2024-04-02 00:19:39 +0530

    Completing js basics 1 lecture

--- Dot Batch Class/JS 1/logic.js
+++ Dot Batch Class/JS 1/logic.js
@@ -29,5 +29,71 @@
 // ename = 'momi';
 // console.log(ename);
 
-let names = ['vow','hie','yudd','tow'];
-console.log(names[2]);
+// let names = ['vow','hie','yudd','tow'];
+// console.log(names[1]);
+
+// let a = 5;
+// let b = 10;
+// let ans1 = (++a)*(--b);
+// console.log(ans1);
+// let ans2 = (a++)*(--b);
+// console.log(ans2);
+// let ans3 = (++a)*(b--);
+// console.log(ans3);
+// let ans4 = (a++)*(b--);
+// console.log(ans4);
+
+// console.log(4 === 3);
+// const userInput = prompt("Please enter your age:");
+// const age = parseInt(userInput);
+// let statuse = (age >= 18)? 'Yes Vote':'No vote';
+// console.log(statuse);
+// console.log(true && 'man' && 4);
+// const userInput = prompt("Please enter your marks:");
+// const marks = parseInt(userInput);
+// if(marks >= 80)
+// {
+//     console.log('A');
+// }
+// else if (marks > 40)
+// {
+//     console.log('D');
+// }
+// else
+// {
+//     console.log('F');
+// }
+
+// let num = 2;
+// const userInput = prompt('Enter the nmber:');
+// const num = parseInt(userInput);
+// switch(num)
+// {
+//     case 1:console.log('A');
+//     break;
+//     case 2:console.log('B');
+//     break;
+//     case 3:console.log('C');
+//     break;
+//     default:console.log('Wrong Input');
+// }
+
+// for(let i=0; i<5; i++)
+// {
+//     console.log(i);
+// }
+
+// let i=0;
+// while(i<5)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+let i=0;
+do
+{
+    console.log(i);
+    i++;
+}
+while(i<10);
