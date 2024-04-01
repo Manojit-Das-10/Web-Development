e11ee785e380e9735682ef30bae120f15c40e566
Manojit Das <manojitdas180306@gmail.com>
2024-04-01 11:28:09 +0530

    Learning js basics

--- Dot Batch Class/JS 1/logic.js
+++ Dot Batch Class/JS 1/logic.js
@@ -1,1 +1,33 @@
-console.log('Nameste Dunia version');
+// console.log('Hello jee kase ho sare');
+// let a = 10;
+// let b = 5;
+// let c = a + b;
+// console.log('Sum is ' + c);
+
+// var e = 10;
+// var f = 90;
+// var g = e + f;
+// console.log('Sum is ' + g);
+
+// if(true)
+// {
+//     let r = 29;
+//     console.log(r);
+// }
+// // console.log(r);
+
+// if(true)
+// {
+//     var i = 89;
+//     console.log(i);
+// }
+// console.log(i);
+
+// let ename = 170;
+// console.log(ename);
+
+// ename = 'momi';
+// console.log(ename);
+
+let names = ['vow','hie','yudd','tow'];
+console.log(names[2]);
