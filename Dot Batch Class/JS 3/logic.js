c58de37e5a5d3ca9a4bc5b6632d138a44b414fed
Manojit Das <manojitdas180306@gmail.com>
2024-04-08 00:15:23 +0530

    Creating js3

---
+++ Dot Batch Class/JS 3/logic.js
@@ -0,0 +1,10 @@
+// console.log(Math.random());
+// console.log(Math.round(1.5));
+// String types:
+// primitive
+let lastName = ' Babbar ';
+// object
+let lastName1 = new String ('madboy');
+let message = 'This is my foster messagae';
+let words = message.split(' ');
+console.log(words);
