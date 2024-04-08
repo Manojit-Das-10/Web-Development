c4b5db97510e236e84d910032add6f45132ded10
Manojit Das <manojitdas180306@gmail.com>
2024-04-09 00:34:12 +0530

    js 3

--- Dot Batch Class/JS 3/logic.js
+++ Dot Batch Class/JS 3/logic.js
@@ -5,6 +5,11 @@
 let lastName = ' Babbar ';
 // object
 let lastName1 = new String ('madboy');
-let message = 'This is my foster messagae';
-let words = message.split(' ');
-console.log(words);
+// let message = 'This is my foster messagae';
+// let words = message.split(' ');
+// console.log(words);
+let message = 
+`${lastName} is 
+my foster
+messagae`;
+console.log(message);
