e7485edfe103072436e395487221c13dd0c0c560
Manojit Das <manojitdas180306@gmail.com>
2024-04-18 00:14:01 +0530

    completing js 3

--- Dot Batch Class/JS with DOM 1/logic.js
+++ Dot Batch Class/JS with DOM 1/logic.js
@@ -3,10 +3,46 @@
 //     console.log('I clicked the website');
 // })
 
-function eventFunction(){
-    console.log('I have clicked on the document');
-}
+// function eventFunction(){
+//     console.log('I have clicked on the document');
+// }
 
-document.addEventListener('click',eventFunction);
+// document.addEventListener('click',eventFunction);
 
 // document.removeEventListener('click',eventFunction);
+
+// let content =  document.querySelector('#para');
+
+// content.addEventListener('click',function(event){
+//     console.log(event);
+// }) 
+
+// let links = document.querySelectorAll('a');
+// let thirdlink = links[1];
+// thirdlink.addEventListener('click',function(event){
+//     event.preventDefault();
+//     console.log('Maja aa gaya');
+// })
+
+// let myDiv = document.createElement('div');
+
+// function paraStatus(event){
+//     console.log('Para'+ event.target.textContent);
+// }
+// myDiv.addEventListener('click',paraStatus);
+
+// for(let i=0; i<=100; i++){
+//     let newElement = document.createElement('p');
+//     newElement.textContent = 'This is para ' + i;
+
+//     myDiv.appendChild(newElement);
+// }
+// document.body.appendChild(myDiv);
+
+let element = document.querySelector('#wrapper');
+
+element.addEventListener('click',function(event){
+    if(event.target.nodeName === 'SPAN'){
+        console.log('You click on ' + event.target.textContent);
+    }
+})
