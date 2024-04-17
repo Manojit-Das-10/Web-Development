f3f6a6ad33f9ced49f3edb746dfb9ffb52ad651e
Manojit Das <manojitdas180306@gmail.com>
2024-04-17 19:31:58 +0530

    learning js eventlistener

---
+++ Dot Batch Class/JS with DOM 1/logic.js
@@ -0,0 +1,12 @@
+
+// document.addEventListener('click',function(){
+//     console.log('I clicked the website');
+// })
+
+function eventFunction(){
+    console.log('I have clicked on the document');
+}
+
+document.addEventListener('click',eventFunction);
+
+// document.removeEventListener('click',eventFunction);
