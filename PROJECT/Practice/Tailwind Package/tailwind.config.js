8fb5229e45db9c3369fd8ca3df3a9252b2ede0ab
Manojit Das <manojitdas180306@gmail.com>
2024-05-01 19:19:59 +0530

    Update

---
+++ PROJECT/Practice/Tailwind Package/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  "scripts": {
+    "start": "vite"
+   },
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
