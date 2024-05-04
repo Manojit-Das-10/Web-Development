65152cbe690134231148636cf4844828c7c5c84e
Manojit Das <manojitdas180306@gmail.com>
2024-05-04 23:12:20 +0530

    adding js

--- PROJECT/Password Generator/logic.js
+++ PROJECT/Password Generator/logic.js
@@ -15,4 +15,70 @@ const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 let password = "";
 let passwordLength = 10;
 let checkCount = 1;
+handleSlier();
 // strength color gray by default
+
+// Set passwordLength
+function handleSlier(){
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
+}
+
+// Indicator
+function setindicator(color){
+    indicator.style.backgroundcolor = color;
+    // shadow
+    indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
+}
+
+// Random Integer
+function getRndInterger(){
+    Math.floor(Math.random() * (max-min)) + min;
+}
+
+function generateRandomNumber(){
+    return getRndInterger(0,9);
+}
+
+function generateLowerCase(){
+   return String.fromCharCode(getRndInterger(97,123));
+}
+
+function generateUpperCase(){
+    return String.fromCharCode(getRndInterger(65,91));
+}
+
+function generateSymbol(){
+    const randNum = getRndInterger(0,symbols.length());
+    return symbols.charAt(randNum);
+}
+
+// Checkbox
+function calcStrength(){
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+    if (uppercaseCheck.checked) hasUpper = true;
+    if (lowercaseCheck.checked) hasLower = true;
+    if (numbersCheck.checked) hasNum = true;
+    if (symbolsCheck.checked) hasSym = true;
+  
+    if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+      setIndicator("#0f0");
+    } 
+    else if (
+      (hasLower || hasUpper) &&
+      (hasNum || hasSym) &&
+      passwordLength >= 6) {
+      setIndicator("#ff0");
+    } 
+    else {
+      setIndicator("#f00");
+    }
+
+}
+
+function copyContent(){
+    
+}
