e4d23b9e22578ebe99b9fd300db22cc839ae4b6d
Manojit Das <manojitdas180306@gmail.com>
2024-07-02 18:58:37 +0530

    demoshopp

--- Dot Batch Class/React Basics/demoshoopp/src/components/ProductForm.js
+++ Dot Batch Class/React Basics/demoshoopp/src/components/ProductForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import './ProductForm'
-
+import './ProductForm.css'
+ 
 function ProductForm(props) {
 
     const [newTitle, setTitle] = useState('');
@@ -12,8 +12,9 @@ function ProductForm(props) {
     }
     function dateChangeHandler(event) {
         setDate(event.target.value);
+        console.log(newTitle);
+        console.log(newDate);
     }
-
 
     function submitHandler(event) {
         event.preventDefault();
@@ -38,7 +39,7 @@ function ProductForm(props) {
         </div>
         <div className='new-product_control'>
             <label>Date</label>
-            <input type='date' value={newDate} onChange={dateChangeHandler} min='2023-01-01' max='2023-12-12'></input>
+            <input type='date' value={newDate} onChange={dateChangeHandler} min='2024-01-01' max='2024 -12-12'></input>
         </div>
         <div className='new-product_button'>
             <button type='submit'>Add Product</button>
