87e280ce450ea5fa6a4007751bc5fb9027f9f476
Manojit Das <manojitdas180306@gmail.com>
2024-07-03 20:05:59 +0530

    Building a react project of a counter button

---
+++ Dot Batch Class/React Basics/counterbutton/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-black">
+      <h2 className="text-4xl text-center text-blue-400">Hello World</h2>
+    </div>
+  );
+}
+
+export default App;
