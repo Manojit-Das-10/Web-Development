3c00ea2bcb878bfda3f4872a149c7c60f824f451
Manojit Das <manojitdas180306@gmail.com>
2024-09-21 11:25:48 +0530

    creating a calculator app

---
+++ PROJECT/Calculator/script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function appendCharacter(character) {
+    display.value += character;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
