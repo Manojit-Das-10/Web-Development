1af9b5de4884f45fbeed48f883c1e988a7dd0ac1
Manojit Das <manojitdas180306@gmail.com>
2024-11-23 19:13:46 +0530

    Small Updates

--- Dot Batch Class/BackEnd/BlogApp/controllers/likeController.js
+++ Dot Batch Class/BackEnd/BlogApp/controllers/likeController.js
@@ -1,3 +1,71 @@
+// import models
+const Post = require("../models/postModel");
+const Like = require("../models/likeModel");
+
+// Like a post
+exports.likePost = async (req,res) => {
+
+    try{
+        const {post,user} = req.body;
+
+        // Check if the post exists in the database
+        const foundPost = await Post.findById(post);
+        if (!foundPost) {
+            return res.status(404).json({
+                error: "Post not found",
+            });
+        }
+
+        const like = new Like({
+            post,user,
+        });
+        const savedLike = await like.save();
+
+        // update the post colection basis on this
+        const updatedPost = await Post.findByIdAndUpdate(post, {$push:{likes:savedLike._id}}, {new:true}).
+        populate("likes").exec();
+        
+        res.json({
+            post:updatedPost,
+        })
+    }
+    catch(error){
+        return res.status(400).json({
+            error:"Error while liking post",
+        });
+    }
+}
+
+// Unlike a post
+exports.unlikePost = async (req,res) => {
+    try{
+        const{post,like} = req.body;
+        // find and delete the like collction me se
+        const deletedLike = await Like.findOneAndDelete({post:post,_id:like});
+
+         // Check if the like was found and deleted
+         if (!deletedLike) {
+            return res.status(404).json({
+                error: "Like not found",
+            });
+        }
+
+        // update the post collection
+        const updatedPost = await Post.findByIdAndUpdate(post, {$pull: {likes: deletedLike._id}}, {new: true});
+        
+        res.json({
+            post:updatedPost,
+        });
+    }
+
+    catch(error){
+        return res.status(400).json({
+            error:"Error while unliking post",
+        });
+    }
+}
+
+
 
 exports.dummyLink = (req, res) => {
     res.send("This is a dummy link response");
--- Dot Batch Class/BackEnd/BlogApp/routes/blog.js
+++ Dot Batch Class/BackEnd/BlogApp/routes/blog.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 // Import controller
-const { dummyLink } = require("../controllers/LikeController");
+const { dummyLink , likePost , unlikePost} = require("../controllers/likeController");
+const {createComment} = require("../controllers/commentController");
+const { createPost , getAllPosts} = require("../controllers/postController");
 
 // Define route
 router.get("/dummyroute", dummyLink);
+router.post("/comments/create", createComment);
+router.post("/posts/create",createPost);
+router.get("/posts",getAllPosts);
+router.post("/likes/like",likePost);
+router.post("/likes/unlike",unlikePost);
 
 // Export the router
 module.exports = router;
